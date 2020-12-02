19b79b9fba656d8fe3b9068ad725393e6477a334
amoshet <ahmed.moshet@gmail.com>
2020-12-02 06:57:47 +0000

    update

--- bankproj/lib/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-//this file you'll have to create directly on the VM or use FTP to move it
-//this should not and will not be on github, -points if it's seen on github.
-
-$dbhost = "localhost";//this should be fine for all
-$dbuser = "am2756";//your ucid since we follow this pattern
-$dbpass = "pWEB6CM/pkcShdXi";//database password generated or chosen per respective steps
-$dbdatabase = "am2756";//your ucid since we follow this pattern
-?>
