df2d59aa67ac6f19cf3932c9356d35fe2426426d
amoshet <ahmed.moshet@gmail.com>
2020-12-15 19:22:44 +0000

    working on dropdown

--- bankproj/transaction.php
+++ bankproj/transaction.php
@@ -12,13 +12,21 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 // preparing account id and accounts for dropdown list
-$db = getDB();
-$stmt = $db->prepare("SELECT id, account_number from Accounts LIMIT 10");
+$srcID = get_user_id();
+$db = getDB();      //if they have the same user ID, give me their accout number and ID
+$stmt = $db->prepare("SELECT id, account_number from Accounts WHERE srcID=:user_id LIMIT 10");
 $r = $stmt->execute();
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 // TODO Add dropdown list
-<form method="POST">
+<form method="POST"> //working on dropdown list
+	<label for "account1"><h3> Account</h3></label>
+	<select name="000000" id="world">
+	  <?php foreach($results):?>
+	  <option value=$results["id"]>$results["account_number"]</option>
+	  <option value="36">123445345436</option>
+	  <?php endforeach;?>
+	</select>
 	<input type="text" name="account1" placeholder="Source Account Number">
 	<!-- If our sample is a transfer show other account field-->
 	<?php if($_GET['type'] == 'transfer') : ?>
